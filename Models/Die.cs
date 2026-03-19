2ac94d8df58dce20126394465372eb4ed3e18d4b
008kevin <kardoskevin07@gmail.com>
2026-03-19 21:45:25 +0100

    style changes

--- Models/Die.cs
+++ Models/Die.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Dobokocka.Models
 {
     public class Die
     {
+        private Brush[] _colors = new Brush[] { Brushes.NavajoWhite, Brushes.LightGreen, Brushes.PaleVioletRed, Brushes.WhiteSmoke, Brushes.Wheat, Brushes.LightBlue };
+
         private static Random r = new Random();
 
         public int Value { get; set; } = 1;
+        public Brush Color { get; set; }
+
+        public Die() {
+            Color = _colors[r.Next(0, _colors.Length)];
+        }
+
         public int Roll()
         {
             Value = r.Next(1, 7);
